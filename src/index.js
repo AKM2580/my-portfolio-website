5315683d100b2495b2ea70d5652ad8b1f5367bc0
AKM2580 <aungkaungmyat29415@gmail.com>
2025-05-24 17:53:21 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Assuming App.js is in the same directory
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
